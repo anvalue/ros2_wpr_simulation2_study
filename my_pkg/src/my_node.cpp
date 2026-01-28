281f74e8878f14a89f6eb610f8785ebddff7cbd5
anvalue <constsheng@gmail.com>
2026-01-28 18:58:01 +0800

    update：重装scode，补充第一份代码的中文注释

--- my_pkg/src/my_node.cpp
+++ my_pkg/src/my_node.cpp
@@ -2,15 +2,21 @@
 
 int main(int argc,char * argv[])
 {
+    // 初始化
     rclcpp::init(argc,argv);
-     
+    
+    // 创建节点
     auto node = std::make_shared<rclcpp::Node>("my_node");
 
+    // 打印日志信息
     RCLCPP_INFO(node->get_logger(),"hello world!");
+
+    // 保持节点运行
     while(rclcpp::ok()){
         ;
     }
 
+    // 关闭rclcpp
     rclcpp::shutdown();
 
     return 0;
